765ca8b4c31aadf97a3ea15195dc2d29bbd3eea8
Connor G. <connor.g7@icloud.com>
2024-04-24 23:45:11 -0500

    Previously was able to make aone surface be a random color. Trying to make a loop so that it generates multiple surfaces, that are each a random color.

--- proposal copy.py
+++ proposal copy.py
@@ -1,4 +1,7 @@
 import pygame
+import random
+import time
+
     #that 3 by 3 artwork will be used as a pattern to create the 6 by 6 artwork.
     #I will reflect and translate the each of the 3/3 to create a "mirrored" piece of generative art that looks intentional,
     #       rather than by a human
@@ -30,9 +33,10 @@ def main():
     ...
     #create a 6 by 6 pixel/square artwork that is randomly generated.
     #I would like a 3 by 3 pixel/square artwork to be randomly generated using two different colors.
-    pygame.init()
+    pygame.init() 
     pygame.display.set_caption("Pixel_Art_Generation")
     clock = pygame.time.Clock()
+    past_time = time.time
     dt = 0
     resolution = (800, 600)
     screen_info = pygame.display.Info()
@@ -42,13 +46,39 @@ def main():
 
     #Background = pygame.image.load("__")
 
-
+    red_value = random.randrange(0, 256)
+    green_value = random.randrange(0, 256)
+    blue_value = random.randrange(0, 256)
     running = True
     while running:
         #Event Loop
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
+        # TODO: game logic
+        #render game here
+        screen.fill('blue')
+        #create a loop to be repeated 3 times. Each time, the surface moves another surface length to the right.
+        for cube in range(1, 4):
+            surf = pygame.Surface((50, 50))
+            surf_width = surf.get_width
+            surf_height = surf.get_height
+            #surf_new_width = int(surf_width + )
+            pix_color = pygame.Color(red_value, green_value, blue_value)
+            #change "pix_color" function to randomize the color values.
+            surf.fill(pix_color)
+            screen.blit(surf, float(100 + [surf_width * cube], 100 + [surf_height * cube]))
+
+        #surf1_2 = pygame.Surface((50, 50))
+        #pix_color = pygame.Color(red_value, green_value, blue_value)
+        #change "pix_color" function to randomize the color values.
+        #surf1_2.fill(pix_color)
+        #screen.blit(surf1_2, (150, 100))
+
+
+        pygame.display.flip()
+        clock.tick(60)
+    pygame.quit()
 
 if __name__ == "__main__":
     main()
